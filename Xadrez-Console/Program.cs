39a6c84ad1df58ae1fa7627a984c50b889fef246
Cesar Coutinho <cesarcoutinho31@gmail.com>
2023-06-11 17:55:35 -0300

    add posicao

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Xadrez_Console
+﻿using Tabuleiro;
+namespace Xadrez_Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Posicao P = new Posicao(1,1);
+            Console.WriteLine($"Posição {P}");
+            Console.ReadKey();
         }
     }
 }
