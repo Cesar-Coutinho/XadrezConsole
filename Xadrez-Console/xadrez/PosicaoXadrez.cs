89954c624c2c6983a83dd04422d5f194921ecd0d
Cesar Coutinho <cesarcoutinho31@gmail.com>
2023-06-11 21:12:41 -0300

    Criando posição xadrez

---
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public char linha { get; set; }
+
+        public PosicaoXadrez(char coluna, char linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
